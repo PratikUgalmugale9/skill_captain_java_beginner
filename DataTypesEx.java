324a11512ed1b0978f67fecab2cad62abea1abf0
PratikUgalmugale9 <147143022+PratikUgalmugale9@users.noreply.github.com>
2023-12-23 20:38:04 +0530

    Add files via upload

---
+++ DataTypesEx.java
@@ -0,0 +1,19 @@
+public class DataTypesEx {
+    public static void main(String[] args) {
+        int myint =42;
+        
+        double myDob =3.14159;
+        
+        char mychar= 'A';
+        
+        boolean mybool =true;
+        
+        var X ="Hello,Skill Caption!";
+        
+        System.out.println("Integer Value :"+myint);
+        System.out.println("Double Variable :"+myDob);
+        System.out.println("Character Variable :"+mychar);
+        System.out.println("Boolean Variable :"+mybool);
+        System.out.println("String Variable :"+X);
+    }
+}
