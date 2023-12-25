79fd6a7fb0dc6d5cdc693b169bd7a56af5df6678
PratikUgalmugale9 <147143022+PratikUgalmugale9@users.noreply.github.com>
2023-12-25 19:00:34 +0530

    Add files via upload

---
+++ PriDataType.java
@@ -0,0 +1,9 @@
+public class PriDataType{
+
+    public static void main(String[] args) {
+        int age = 25;
+        String name = "Skill Caption!";
+
+        System.out.println("Hello " +name +"You are "+age+" years old.");
+    }
+}
