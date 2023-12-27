1ec94ac755373d6a7811d7998792537963972e5a
PratikUgalmugale9 <147143022+PratikUgalmugale9@users.noreply.github.com>
2023-12-27 21:26:09 +0530

    Add files via upload

---
+++ Conditions.java
@@ -0,0 +1,15 @@
+public class Conditions {
+
+    public static void main(String[] args){
+
+        int num1=10;
+        int num2=20;
+
+        if (num1 > num2) {
+            System.out.println("The Larger number is :"+num1);
+        }
+        else {
+            System.out.println("The Larger number is :"+num2);
+        }
+    }
+}
